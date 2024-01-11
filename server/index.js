07339af831f071d860111727cae2321861ae2709
gayatrisathawane <gayatripsathawane@gmail.com>
2024-01-11 19:43:40 +0530

    socket.io feature implemented in backend

--- server/index.js
+++ server/index.js
@@ -1,20 +1,42 @@
 import express from 'express'
-import  mongoDB from './db/db.js'
+import mongoDB from './db/db.js'
 import dotenv from 'dotenv'
 dotenv.config();
-
-import { signupPostApi,loginPostApi } from './controller/user.controller.js'
+import { Server } from 'socket.io'
+import { signupPostApi, loginPostApi } from './controller/user.controller.js'
 
 const app = express();
 app.use(express.json())
 const PORT = process.env.PORT || 5000
 
 
-//user Login and SignUp
-app.post('/api/v1/signups',signupPostApi )
-app.post('/api/v1/logins',loginPostApi )
+const io = new Server(5002, {
+    cors: {
+        origin: '*'
+    }
+})
+//socket connection
+io.on("connection", (socket) => {
+    socket.emit("user connected");
+    socket.on("message", (data) => {
+        console.log(data)
+    })   //"message is event name"
+})
 
-app.listen(PORT,()=>{
+//reciever api 
+
+app.get('/sends', async (req, res) => {
+    const { message } = req.query
+    io.emit("recieve", message)
+    res.json({
+        message: "Message sent"
+    })
+})
+//user Login and SignUp
+app.post('/api/v1/signups', signupPostApi)
+app.post('/api/v1/logins', loginPostApi)
+
+app.listen(PORT, () => {
     console.log(`port is running on port ${PORT}`)
     mongoDB()
 })
